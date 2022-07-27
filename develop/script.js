2e3beb56349a66227514d7595b27a34f0fe28813
Tara Evans <tarajevans@hotmail.com>
2022-07-26 21:06:57 -0400

    geocoding and weather api

---
+++ develop/script.js
@@ -0,0 +1,40 @@
+var currentLat;
+var currentlng;
+var city;
+var date;
+var icon;
+var temp;
+var humidity;
+var wind_speed;
+var uvi;
+var cityIn="Peterborough";
+var stateIn="Ontario";
+
+
+function getCords(){
+    fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + cityIn + ", + " + stateIn + "&key=AIzaSyAweIP9TjVJjtUjpbZuiu69BBoh9sMPMUc").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data.results[0].geometry.location.lat)
+            currentLat = data.results[0].geometry.location.lat.toFixed(6);
+            currentlng = data.results[0].geometry.location.lng.toFixed(6);
+            city = data.results[0].address_components[0].long_name;
+            getWeather();
+        });
+    });
+}
+
+function getWeather(){
+    fetch("https://api.openweathermap.org/data/3.0/onecall?lat=" + currentLat + "&lon=" + currentlng + "&units=metric&appid=ec30fe20fd671a00bbcc63e83e68cb73").then(function(response) {
+            response.json().then(function(data) {
+                temp=data.current.temp;
+                humidity=data.current.humidity;
+                wind_speed=data.current.wind_speed;
+                uvi=data.current.uvi;
+                console.log(temp + " " + humidity + " " + wind_speed + " " + uvi + " " + city);
+                console.log(data);
+            
+        });
+    });
+}
+
+getCords();
