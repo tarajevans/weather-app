47278b73719e06f6fae110e29eeb3cb0b4d0adae
Tara Evans <tarajevans@hotmail.com>
2022-07-30 07:44:35 -0400

    added icons and some html and css

--- develop/script.js
+++ develop/script.js
@@ -2,7 +2,7 @@ var currentLat;
 var currentlng;
 var city;
 var date;
-var icon;
+var currentIcon;
 var temp;
 var humidity;
 var wind_speed;
@@ -11,19 +11,37 @@ var cityIn="Peterborough";
 var stateIn="Ontario";
 var userCity=document.getElementById("cityIn");
 var userState=document.getElementById("stateIn");
+var displayResults=document.getElementById("displayResults");
 var places=[];
 
-
+//city input form
 $("#search").click(function(){
     cityIn = userCity.value;
     stateIn = userState.value;
     var tempArray = [cityIn, stateIn];
     places.push(tempArray);
-    
+    //save to local storage
     localStorage.setItem("cities", JSON.stringify(places));
+    //retrieve local storage
+    function getHistory() {
+        var result=localStorage.getItem("cities");
+        if (result!=null){
+            var parseResult=JSON.parse(result);
+                for(var i=0; i>parseResult.length; i++){
+                    places.push(parseResult[i]);
+                }
+        }else{
+            console.log("cities not found");
+        }
+        console.log(places);
+    }
+    getHistory();
+    getCords();
 });
 
 
+
+//turns search string into lat+long coords
 function getCords(){
     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + cityIn + ", + " + stateIn + "&key=AIzaSyAweIP9TjVJjtUjpbZuiu69BBoh9sMPMUc").then(function(response) {
         response.json().then(function(data) {
@@ -36,6 +54,7 @@ function getCords(){
     });
 }
 
+//gets response from weather api
 function getWeather(){
     fetch("https://api.openweathermap.org/data/3.0/onecall?lat=" + currentLat + "&lon=" + currentlng + "&units=metric&appid=ec30fe20fd671a00bbcc63e83e68cb73").then(function(response) {
             response.json().then(function(data) {
@@ -43,11 +62,19 @@ function getWeather(){
                 humidity=data.current.humidity;
                 wind_speed=data.current.wind_speed;
                 uvi=data.current.uvi;
+                currentIcon = data.current.weather[0].icon;
                 console.log(temp + " " + humidity + " " + wind_speed + " " + uvi + " " + city);
                 console.log(data);
+                function displayWeather(){
+                    var resultString= city + " " + date + " " +  currentIcon + " "  + temp + " " + humidity + " " + wind_speed + " " + uvi;
+                    displayResults.textContent=resultString;
+                  }
+                displayWeather();
+                displayResults.innerHTML="<img src=icons/"+currentIcon+".png>"
             
         });
     });
 }
 
 getCords();
+
